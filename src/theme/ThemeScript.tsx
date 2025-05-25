0a2e6cd48e4e38a9fb0d51238493661355540698
Chinmay Tawade <chinmay.tawade24@spit.ac.in>
2025-05-26 02:54:24 +0530

    Create ThemeScript.tsx

---
+++ src/theme/ThemeScript.tsx
@@ -0,0 +1,13 @@
+export function ThemeScript() {
+  const script = `
+    (function() {
+      const theme = localStorage.getItem('theme');
+      if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    })();
+  `;
+  return <script dangerouslySetInnerHTML={{ __html: script }} />;
+}
