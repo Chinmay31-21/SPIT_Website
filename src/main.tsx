0a7fb6585757b0e7caf3bb501948009728a30ecd
Chinmay Tawade <chinmay.tawade24@spit.ac.in>
2025-05-26 09:28:20 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// 👇 Add these two lines:
-import { ThemeProvider } from './theme/ThemeProvider';
-import { ThemeScript } from './theme/ThemeScript';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ThemeScript />
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
