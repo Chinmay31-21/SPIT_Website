ef1a9de30299cb58caef7bf267946b3dda8b2787
Chinmay Tawade <chinmay.tawade24@spit.ac.in>
2025-05-27 11:58:44 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </StrictMode>
+  </ThemeProvider>
+</React.StrictMode>
+
 );
