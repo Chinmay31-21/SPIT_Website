df7b40abaf22225f407309a2f14c54507c1833fd
sanyogeetapradhan <sanyogeetapradhan@gmail.com>
2025-06-30 00:52:47 +0530

    Research.tsx(50%)

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import App from './App';
 import { ThemeProvider } from './ThemeProvider';
 import './index.css';
 
+const ErrorBoundary = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </React.Suspense>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
